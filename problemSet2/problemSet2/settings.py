9d9de71045bf03e98fd7101c448f4adfa905b348
Tapple214 <applegate21441@gmail.com>
2026-09-24 16:27:26 +0700

    yml

--- problemSet2/problemSet2/settings.py
+++ problemSet2/problemSet2/settings.py
@@ -78,8 +78,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME', 'studentdb'),
+        'USER': os.environ.get('DB_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'postgres'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
